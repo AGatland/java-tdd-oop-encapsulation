d0aa22844f1316e950adb7c09903217336ebc0ce
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-17 10:55:15 +0100

    Create tests for choosing colour of RC

--- src/test/java/com/booleanuk/core/RemoteControlledCarTest.java
+++ src/test/java/com/booleanuk/core/RemoteControlledCarTest.java
@@ -1,7 +1,22 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RemoteControlledCarTest {
+
+    @Test
+    public void testChooseColorForRC() {
+        RemoteControlledCar rc = new RemoteControlledCar("Red");
+        Assertions.assertEquals("Red", rc.colour);
+    }
+
+    @Test
+    public void testChooseColorForRCWrongColour() {
+        RemoteControlledCar rc = new RemoteControlledCar("Red");
+        Assertions.assertNotEquals("White", rc.colour);
+    }
 
 }
