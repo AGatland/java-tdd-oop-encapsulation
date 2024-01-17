47f5313bd3b0e2c7d24d8f557f7221cca9df70f8
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-17 11:07:08 +0100

    Implement code for choosing remote type

--- src/main/java/com/booleanuk/core/RemoteControlledCar.java
+++ src/main/java/com/booleanuk/core/RemoteControlledCar.java
@@ -3,9 +3,11 @@ package com.booleanuk.core;
 public class RemoteControlledCar {
     String colour;
     Battery battery;
+    String remoteType;
 
-    public RemoteControlledCar(String colour, String batteryType) {
+    public RemoteControlledCar(String colour, String batteryType, String remoteType) {
         this.colour = colour;
         this.battery = new Battery(batteryType);
+        this.remoteType = remoteType;
     }
 }
