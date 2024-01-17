edbf2750ca7c89423662f2c93abdae34ae219110
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-17 11:06:17 +0100

    Implement tests for choosing remote type

--- src/test/java/com/booleanuk/core/RemoteControlledCarTest.java
+++ src/test/java/com/booleanuk/core/RemoteControlledCarTest.java
@@ -9,26 +9,44 @@ class RemoteControlledCarTest {
 
     @Test
     public void testChooseColorForRC() {
-        RemoteControlledCar rc = new RemoteControlledCar("Red", "disposable");
+        RemoteControlledCar rc = new RemoteControlledCar("Red", "disposable", "simple");
         Assertions.assertEquals("Red", rc.colour);
     }
 
     @Test
     public void testChooseColorForRCWrongColour() {
-        RemoteControlledCar rc = new RemoteControlledCar("Red", "disposable");
+        RemoteControlledCar rc = new RemoteControlledCar("Red", "disposable", "simple");
         Assertions.assertNotEquals("White", rc.colour);
     }
 
     @Test
     public void testChooseDisposableBatteryTypeForRC() {
-        RemoteControlledCar rc = new RemoteControlledCar("Red", "disposable");
+        RemoteControlledCar rc = new RemoteControlledCar("Red", "disposable", "simple");
         Assertions.assertEquals("disposable", rc.battery.type);
     }
 
     @Test
     public void testChooseRechargableBatteryTypeForRC() {
-        RemoteControlledCar rc = new RemoteControlledCar("Red", "rechargable");
+        RemoteControlledCar rc = new RemoteControlledCar("Red", "rechargable", "simple");
         Assertions.assertEquals("rechargable", rc.battery.type);
+    }
+
+    @Test
+    public void testChooseAdvancedRemoteTypeForRC() {
+        RemoteControlledCar rc = new RemoteControlledCar("Red", "disposable", "advanced");
+        Assertions.assertEquals("advanced", rc.remoteType);
+    }
+
+    @Test
+    public void testChooseSimpleRemoteTypeForRC() {
+        RemoteControlledCar rc = new RemoteControlledCar("Red", "disposable", "simple");
+        Assertions.assertEquals("simple", rc.remoteType);
+    }
+
+    @Test
+    public void testChooseRemoteTypeForRCWrongType() {
+        RemoteControlledCar rc = new RemoteControlledCar("Red", "disposable", "simple");
+        Assertions.assertNotEquals("advanced", rc.remoteType);
     }
 
 }
